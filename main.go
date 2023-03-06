041c20c1625f9e393fbc65e89dfd1852483ddb7e
Commodus <nick.v.castelli@gmail.com>
2023-03-05 22:46:46 -0600

    Fixed issue with averaging speeds

--- main.go
+++ main.go
@@ -22,52 +22,53 @@ func main(){
    //scanLengthSeconds := int(d / time.Second)
 
 
-   var sum float64 = 0;
+   var sum float64 = 0
    var downloadSpeeds []float64
 
 
    for stay, timeout := true, time.After(timeLength); stay; {
-    fmt.Println(time.Now());
-    select {
-    case <-timeout:
-        stay = false
-	fmt.Println("+++I'm in timeout case?+++")
-    default:
-	 fmt.Println("---I'm in default case?---")
-    
+      fmt.Println(time.Now());
+      select {
+         case <-timeout:
+            stay = false
+            fmt.Println("+++I'm in timeout case?+++")
+         default:
+   	      fmt.Println("---I'm in default case?---")
 
+            user, _ := st.FetchUserInfo()
+         	// Get a list of servers near a specified location
+         	// user.SetLocationByCity("Tokyo")
+         	// user.SetLocation("Osaka", 34.6952, 135.5006)
 
-   user, _ := st.FetchUserInfo()
-	// Get a list of servers near a specified location
-	// user.SetLocationByCity("Tokyo")
-	// user.SetLocation("Osaka", 34.6952, 135.5006)
+         	serverList, _ := st.FetchServers(user)
+         	targets, _ := serverList.FindServer([]int{})
 
-	serverList, _ := st.FetchServers(user)
-	targets, _ := serverList.FindServer([]int{})
+         	for _, s := range targets {
+         		s.PingTest()
+         		s.DownloadTest(false)
+         		s.UploadTest(false)
 
-	for _, s := range targets {
-		s.PingTest()
-		s.DownloadTest(false)
-		s.UploadTest(false)
+         		fmt.Printf("Latency: %s, Download: %f, Upload: %f\n", s.Latency, s.DLSpeed, s.ULSpeed)
+         		downloadSpeeds = append(downloadSpeeds, s.DLSpeed)
+         		count++
+         	}
 
-		fmt.Printf("Latency: %s, Download: %f, Upload: %f\n", s.Latency, s.DLSpeed, s.ULSpeed)
-		downloadSpeeds = append(downloadSpeeds, s.DLSpeed)
-		count++
-	}
-
-   for _, value := range downloadSpeeds {
-	   sum += value
-   }
-
-   fmt.Println("The list of speeds are: ")
-   fmt.Println(downloadSpeeds)
+            fmt.Println("The list of speeds are: ")
+            fmt.Println(downloadSpeeds)
 
       }
 
-      averageSpeeds := sum/count
-      fmt.Println("The download speed average is:")
-   fmt.Println(averageSpeeds)
-
    }
+
+   for _, value := range downloadSpeeds {
+      sum += value
+   }
+
+   fmt.Println("The number of PINGs ran are:")
+   fmt.Println(count)
+
+   averageSpeeds := sum/count
+   fmt.Println("The download speed average is:")
+   fmt.Println(averageSpeeds)
 
 }
